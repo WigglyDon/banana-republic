4b40dac33b50145e01693a2cb0aecf7079fd03c2
WigglyDon <donfather1000@gmail.com>
2023-12-20 14:47:33 -0700

    update home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Bananies! :)</h1>`,
+  imports: [
+    HomeComponent,
+  ],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg"
+      alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
