0f56a33d8566af2091e1f02849ae7664f3246363
WigglyDon <donfather1000@gmail.com>
2023-12-20 15:44:39 -0700

    add banana location and styles

---
+++ src/app/banana-location/banana-location.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-banana-location',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      banana-location works!
+    </p>
+  `,
+  styleUrl: './banana-location.component.css'
+})
+export class BananaLocationComponent {
+
+}
--- src/app/banana/banana.component.ts
+++ src/app/banana/banana.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BananaLocationComponent } from '../banana-location/banana-location.component';
 
 @Component({
   selector: 'app-banana',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    BananaLocationComponent,
+  ],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filter by banana">
         <button class="primary" type="button">Search</button>
       </form>
+    </section>
+
+    <section class="results">
+      <app-banana-location></app-banana-location>
     </section>
   `,
   styleUrl: './banana.component.css'
