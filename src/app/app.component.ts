941f29767cd9fa7c735c1feaeb501baae6f86c37
WigglyDon <donfather1000@gmail.com>
2023-12-27 19:43:07 -0700

    finish banana details and add styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,8 @@ import { RouterModule } from '@angular/router';
   <main>
     <a [routerLink]="['/']">
       <header class="brand-name">
-        <img class="brand-logo" src="/assets/banana-logo.svg"
-        alt="logo" aria-hidden="true">
+        <img class="brand-logo" src="/assets/banana-logo.svg" alt="logo" aria-hidden="true">
+        <h2>BANANA REPUBLIC</h2>
       </header>
     </a>
     <section class="content">
--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { BananaService } from '../banana.service';
+import { BananaBunch } from '../bananabunch';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      details works!
-    </p>
+  <article>
+    <img class="banana-photo" [src]="bananaBunch?.photo" alt="missing picture"/>
+    <section class="banana-description">
+      <h2 class="banana-heading">{{bananaBunch?.name}}</h2>
+      <p class="banana-flavor">{{bananaBunch?.flavor}}, {{bananaBunch?.color}}</p>
+    </section>
+    <section class="banana-features">
+      <h2 class="section-heading">About this banana</h2>
+      <ul>
+        <li>Bunch Size: {{bananaBunch?.bunchSize}}</li>
+        <li>Is this genetically altered: {{bananaBunch?.geneticallyAltered}}</li>
+        <li>Is this banana edible: {{bananaBunch?.edible}}</li>
+      </ul>
+    </section>
+  </article>
   `,
   styleUrl: './details.component.css'
 })
-export class DetailsComponent {
 
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  bananaService = inject(BananaService);
+  bananaBunch: BananaBunch | undefined;
+
+  constructor() {
+    const bananaBunchId = Number(this.route.snapshot.params['id']);
+    this.bananaBunch = this.bananaService.getBananaBunchById(bananaBunchId);
+  }
 }
